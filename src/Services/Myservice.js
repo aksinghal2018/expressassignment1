e3cb384aafca198f701754a4be07df6d032a849c
aksinghal2018 <akshay.singhal@krishnacollege.ac.in>
2021-11-25 18:14:41 +0530

    first commit

--- src/Services/Myservice.js
+++ src/Services/Myservice.js
@@ -2,19 +2,19 @@ import axios from 'axios'
 import { MainUrl } from '../config/Url'
 
 export const getrequest=()=>{
-    return(axios.get(`${MainUrl}post/getpost`))
+    return(axios.get(`${MainUrl}post/getpost`,{ withCredentials: true }))
 }
 export  const  postrequest=(data)=>{
     return(
-    axios.post(`${MainUrl}post/addpost`,data))
+    axios.post(`${MainUrl}post/addpost`,data),{ withCredentials: true })
 }
 export  const  updaterequest=(data)=>{
    // console.log(data)
     const config = { headers: {'Content-Type': 'application/json'} };
     return(
-    axios.put(`${MainUrl}post/updatepost`,data,config))
+    axios.put(`${MainUrl}post/updatepost`,data,{ withCredentials: true }))
 }
 export  const  deletetrequest=(data)=>{
     return(
-    axios.delete(`${MainUrl}post/deletepost/${data}`))
+    axios.delete(`${MainUrl}post/deletepost/${data}`),{ withCredentials: true })
 }
