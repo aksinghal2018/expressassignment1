607e7486c36a97d95a11c710f14c006276d2ba49
aksinghal2018 <akshay.singhal@krishnacollege.ac.in>
2021-11-26 21:02:32 +0530

    first commit

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -2,19 +2,67 @@ import React from 'react'
 import { useState, useEffect } from 'react'
 import { getrequestproduct } from '../Services/Myservice'
 function Product() {
+    const [productshow, setproductshow] = useState([])
     const [product, setproduct] = useState([])
+    const [choice, setchoice] = useState("All")
+    const [category, setcategory] = useState([])
     useEffect(() => {
         getrequestproduct().then(
-            data => setproduct(data.data.pdata)
+            data => {
+                var categorydata=[]
+                data.data.pdata.map(
+                    item=>{
+                        //console.log(categorydata.indexOf(item.category)==-1)
+                        if(categorydata.indexOf(item.category)==-1){
+                            categorydata.push(item.category)
+                        }
+                    }
+                )
+                //console.log(categorydata)
+                setproduct(data.data.pdata)
+                setcategory(categorydata)
+            }
         )
     }, [])
+    const handler=(e)=>{
+        setchoice(e.target.value)
+        const value=e.target.value
+        console.log(value)
+        if(value=="All"){
+            setproductshow(product)
+        }
+        else{
+            var data=[]
+            product.map(item=>{
+                if(item.category==value){
+                    data.push(item)
+                }
+            })
+            setproductshow(data)
+        }
+
+        
+    }
     return (
 
         <div className="container mt-4" style={{border:"2px solid black"}}>
-            <h1><u>Products</u></h1>
+            <div style={{display:"flex"}}><h1><u>Products</u></h1>
+            
+            <div  style={{fontSize:"20px",color:"black",marginLeft:"50%",marginTop:"2%"}}><form>
+  <label for="cars">Choose Category:</label>
+  <select id="cars" name="cars" style={{width:"200px"}}   onChange={handler}>
+    <option value="none">None</option>
+    <option value="All">All</option>
+    {
+        category.map(item=>{
+            return(<option value={item}>{item}</option>)
+        })
+    }
+  </select>
+  </form></div></div>
             <div className="row" style={{padding:"20px"}}>
                 {
-                    product.map((item, index) => {
+                    productshow.map((item, index) => {
                         return (
                             <div className="card col-lg-3 col-md-4 col-4 mt-4" style={{width: "18rem",marginRight:"40px",marginLeft:"40px"}}>
                                 <img className="card-img-top mt-2" src={`http://localhost:8899/${item.images}`} alt="Card image cap" />
